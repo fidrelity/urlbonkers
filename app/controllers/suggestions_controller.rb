4cfe6a65434ad9232d059a1b7a8cc5afcad62178
Andre Schweighofer <nopunktfjutscha@gmail.com>
2015-03-17 20:23:29 +0100

    suggestions controller and views

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,8 +1,20 @@
 class SuggestionsController < ApplicationController
 
   def index
+    @suggestion = Suggestion.new
   end
 
   def create
+    @suggestion = Suggestion.new(suggestion_params)
+    if @suggestion.invalid?
+      flash[:error] = @suggestion.errors.full_messages.to_sentence
+      render :index
+    end
+  end
+
+  private
+
+  def suggestion_params
+    params.require(:suggestion).permit(:buzzword)
   end
 end
